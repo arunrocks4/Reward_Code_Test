055136e6e764f00f72ec925b747ecb626c340f87
Admin <29699600+arunrocks4@users.noreply.github.com>
2024-09-04 09:20:34 -0400

    test cases added

--- src/ProductItem.js
+++ src/ProductItem.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const ProductItem = ({ product, addToCart }) => {
   return (
-    <div className="product-item">
+    <div className="product-item" data-testid="productItem">
       <img src={product.thumbnail} alt={product.name} style={{ width: '100px' }} />
       <h3>{product.name}</h3>
       <p>{product.description}</p>
       <p>Price: ${product.price}</p>
-      <button onClick={() => addToCart(product)}>Add to Cart</button>
+      <button data-testid="addtocartbtn" onClick={() => addToCart(product)}>Add to Cart</button>
     </div>
   );
 };
