c2b4c26bf5cdc40ef13c039dc5884c00dceebf00
Admin <29699600+arunrocks4@users.noreply.github.com>
2024-09-04 03:05:55 -0400

    update fetch api call

--- src/App.js
+++ src/App.js
@@ -1,57 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductList from './ProductList';
 import Cart from './Cart';
 
-// Example JSON data
-const productsData = [
-  {
-    id: 1,
-    name: "Product 1",
-    price: 10,
-    thumbnail: "https://example.com/thumb1.jpg",
-    description: "This is a great product"
-  },
-  {
-    id: 2,
-    name: "Product 2",
-    price: 20,
-    thumbnail: "https://example.com/thumb2.jpg",
-    description: "This is another great product"
-  },
-  {
-    id: 3,
-    name: "Product 3",
-    price: 30,
-    thumbnail: "https://example.com/thumb2.jpg",
-    description: "This is another great product"
-  },
-  {
-    id: 4,
-    name: "Product 4",
-    price: 40,
-    thumbnail: "https://example.com/thumb2.jpg",
-    description: "This is another great product"
-  },
-  {
-    id: 5,
-    name: "Product 5",
-    price: 50,
-    thumbnail: "https://example.com/thumb2.jpg",
-    description: "This is another great product"
-  }
-];
+
 
 const App = () => {
+  const [productData, setProductData] = useState([]);
   const [cartItems, setCartItems] = useState([]);
 
   const addToCart = (product) => {
     setCartItems([...cartItems, product]);
   };
 
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then(data => setProductData(data.products));
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App" data-testid="app">
       <h1>Products</h1>
-      <ProductList products={productsData} addToCart={addToCart} />
+      <ProductList products={productData} addToCart={addToCart} />
       <Cart cartItems={cartItems} />
     </div>
   );
